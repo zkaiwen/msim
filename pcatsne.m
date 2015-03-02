b9a36938e54d261b780633d7388a4ebf473b024c
zkaiwen <zkaiwen@gmail.com>
2015-03-02 18:03:03 -0500

    Updated matlab files

--- pcatsne.m
+++ pcatsne.m
@@ -2,15 +2,21 @@ function pcatsne(mat)
 
 %PCA
 pcaresult = pcatest(mat);
+figure
+labels = load('labels.csv');
+labels = labels'
+labelstr = num2str((1:size(mat,1))', '%d');
+scatter3(pcaresult(:,1), pcaresult(:,2),  pcaresult(:,3), 40, labels, 'filled', 'MarkerEdgeColor', [0, 0, 0], 'LineWidth', 1);
+text(pcaresult(:,1), pcaresult(:,2), pcaresult(:,3),  labelstr, 'FontSize', 8)
 
 
 %REDUCE TO 30 DIM THEN RUN TSNE
 no_dims = 2; % supposed number of dimensions
-prp = 30; % perplexity
-mappedX = compute_mapping(pcaresult(:, [1:30]), 't-SNE', no_dims, prp);
+prp = 5; % perplexity
 
+%Take the first 60 dimensions of pca at least handles 95% of variance
+mappedX = compute_mapping(pcaresult(:, [1:60]), 't-SNE', no_dims, prp);
 
-labels = load('labels.csv');
-labelstr = num2str((1:size(mappedX,1))', '%d');
+figure
 scatter(mappedX(:,1), mappedX(:,2), 40, labels, 'filled', 'MarkerEdgeColor', [0, 0, 0], 'LineWidth', 1);
 text(mappedX(:,1), mappedX(:,2), labelstr, 'FontSize', 8);
--- test.m
+++ test.m
@@ -1,6 +1,8 @@
 struct = csvread('birthmark.csv')
 stat = csvread('stat.csv')
-statn = normalize(stat)
-structn = normalize(struct)
-fullbm = cat(2, stat, struct)
-fullbmn = cat(2, statn, structn)
+statn = normalize(stat);
+structn = normalize(struct);
+
+fullbm = cat(2, stat, struct);
+fullbmn = cat(2, statn, structn);
+%fullbmn(:, ~any(fullbmn, 1) ) = [];
